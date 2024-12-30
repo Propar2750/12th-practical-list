686fd592911f6a0d2f9576056d35a6906c3dce0b
Propar2750 <82144119+Propar2750@users.noreply.github.com>
2024-12-31 00:02:27 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,17 +1,23 @@
-def push(item,list_name):
+def push(item, list_name):
     list_name.append(item)
+
+
 def pop(list_name):
     if list_name != []:
         list_name.pop()
     else:
         pass
 
+
 string_1 = list(input("Input the string 1: "))
-string_2= list(input("Input the string 2: "))
-def AreEqual(string_1,string_2):
+string_2 = list(input("Input the string 2: "))
+
+
+def AreEqual(string_1, string_2):
     str_1 = []
     str_2 = []
-    def abc(string_name,final_string):
+
+    def abc(string_name, final_string):
         temp_str_list = []
         for chara in string_name:
             if chara == "#":
@@ -23,12 +29,14 @@ def AreEqual(string_1,string_2):
             final_str += chara
         return final_str
 
-    if abc(string_1,final_string=str_1) ==abc(string_2,final_string=str_2):
+    if abc(string_1, final_string=str_1) == abc(string_2, final_string=str_2):
         return True
     else:
         return False
+
+
 if __name__ == "__main__":
-    print(AreEqual(string_1,string_2))
+    print(AreEqual(string_1, string_2))
 
 '''
 Output:
